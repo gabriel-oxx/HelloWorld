a7a28d35b43af1ae609b7ea578f854907df250c5
Dev-GabCosta <gabriel.costa@zup.com.br>
2024-12-02 13:54:41 -0300

    Criar projeto

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("From Gabriel to Catalisa:");
+		System.out.println("Hello, world! Hello, Zup!");
+		System.out.println("I'm coming!");
+	}
+}
